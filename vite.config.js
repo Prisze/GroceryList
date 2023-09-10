d0f88e8a9e3e1efe462989754e64baaba59bca8b
Priscilla <priscilla.marquez@outlook.com>
2023-09-10 11:05:14 -0500

    deploying

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from'@vitejs/plugin=vue'
+
+// https://vitejs.dev/config/
+
+export default defineConfig ({
+    base: '/deploying-vite-project/',
+    plugins: [vue()]
+})
